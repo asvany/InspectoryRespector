1b47a219d40f5cded8a2a79a0613a66a85f0b63c
as <as@zenstudios.com>
2023-06-15 15:09:28 +0200

    first commmit

---
+++ main.go
@@ -0,0 +1,21 @@
+// this application tracking and logging the user activity
+// periodically poll the keyboard and mouse activity status and detect idle periods and window focus changes
+// if the user is active periodically make a blurred screenshot and stores this and the activity data in a local folder
+
+package main
+
+import "fmt"
+import "log"
+import "github.com/joho/godotenv"
+
+
+// this is the main file and this is the start function of the application
+func main() {
+	fmt.Println("Hello World")
+	err := godotenv.Load("secret.env", "unsecret.env")
+	if err != nil {
+		log.Println("WARRNING: error while loading all env files: ", err)
+	}
+
+	
+}
