279c8f66ea2eb3e2251feca39af001685062bae4
David Agoston <asvany@gmail.com>
2023-07-14 09:00:22 +0200

    fixes

--- jsonify_dump.py
+++ jsonify_dump.py
@@ -7,47 +7,54 @@ from google.protobuf.json_format import MessageToJson
 
 import json
 
+
 def processFile(filename):
-	events=[]
-	with open(filename, "rb") as f:
-		print("Processing file: " + filename)
-		n = 0
-		while True:
-			length_bytes = f.read(4)
-			if len(length_bytes) < 4:
-				print("End of file")
-				break
-			length = int.from_bytes(length_bytes, 'little')
-			print("length: ", length)
-			data = f.read(length)
-			if len(data) < length:
-				print("File is corrupted")
-				break
-			windowChange = WindowChange()
-			windowChange.ParseFromString(data)
-			n += 1
-			#convert the protobuff message to json
-			json_windowChange = MessageToJson(windowChange
-                                     ,including_default_value_fields=True
-                                     ,preserving_proto_field_name=True
-                                     ,indent="\t"
-                                     ,sort_keys=True
-                                     )
-			events.append(json_windowChange)
-			print("no of events: ", n)
-	return events
+    events = []
+    with open(filename, "rb") as f:
+        print("Processing file: " + filename)
+        while True:
+            # read the length of the message
+            length_bytes = f.read(4)
+            if len(length_bytes) == 0:
+                print("End of file")
+                break
+            elif len(length_bytes) < 4:
+                print(
+                    f"File is corrupted: (message length) read:{ len(length_bytes)} excepted:4",
+                )
+                break
+            length = int.from_bytes(length_bytes, "little")
+            # read the message
+            data = f.read(length)
+            if len(data) < length:
+                print(f"File is corrupted (message) read:{len(data)} expected:{length}")
+                break
+            # convert the message from protobuff
+            windowChange = WindowChange()
+            windowChange.ParseFromString(data)
+            # convert the protobuff message to json
+            json_windowChange = MessageToJson(
+                windowChange,
+                including_default_value_fields=True,
+                preserving_proto_field_name=True,
+                indent="\t",
+                sort_keys=True,
+            )
+            events.append(json_windowChange)
+    print("no of events: ", len(events))
+    return events
+
 
 def main():
-	print("Hello World!")
-	# parsing each of the command line arguments as a filename
-	for filename in sys.argv[1:]:
-		events=processFile(filename)
-		s="[\n"
-		for event in events:
-			s+=event+",\n"
-		s=s[:-2]+"]\n"
-		with open(filename+".json", "w") as f:
-			f.write(s)
-		
-if __name__== "__main__":
-	main()
+    print("Hello World!")
+    # parsing each of the command line arguments as a filename
+    for filename in sys.argv[1:]:
+        events = processFile(filename)
+        # concatenate all json string into one json string
+        s = "[" + ",\n".join(events) + "]\n"
+        with open(filename + ".json", "w") as f:
+            f.write(s)
+
+
+if __name__ == "__main__":
+    main()
