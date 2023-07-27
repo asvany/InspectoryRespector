ccf668b8d568922d37b4e266fcdbd4c911f8a042
David Agoston <asvany@gmail.com>
2023-07-27 16:43:14 +0200

    systray rewrite with fyne

--- tray/tray_handler.go
+++ tray/tray_handler.go
@@ -1,111 +1,89 @@
 package tray
 
+// #cgo pkg-config: gtk+-3.0
+
+// https://developer.fyne.io/explore/canvas
+
 import (
 	// "os"
 
 	"log"
-	"runtime"
 	"sync"
 
-	"github.com/dawidd6/go-appindicator"
-	// "github.com/gotk3/gotk3/glib"
 	"github.com/asvany/InspectoryRespector/tracker"
-	"github.com/gotk3/gotk3/glib"
-	"github.com/gotk3/gotk3/gtk"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/driver/desktop"
+	// "fyne.io/fyne/v2/widget"
 )
 
 var icon_file_enabled = "binary/icon_base_2.png"
 var icon_file_disabled = "binary/icon_base_2_bw.png"
 
-// change appindicator icon
-func SetIcon(indicator *appindicator.Indicator, enabled bool) {
-	indicator.SetIconThemePath("./binary")
-	if enabled {
-		indicator.SetIcon(icon_file_enabled)
-		indicator.SetAttentionIcon(icon_file_enabled)
-		indicator.SetIconFull(icon_file_enabled, "enabled")
-	} else {
-		indicator.SetIcon(icon_file_disabled)
-		indicator.SetAttentionIcon(icon_file_disabled)
-		indicator.SetIconFull(icon_file_disabled, "disabled")
-	}
-}
-
 // var image = trayhost.NewImageFromPath(icon_file)
 
 func InitTray(quitChan chan bool, wg *sync.WaitGroup, inputEventCollector *tracker.InputEventCollector) {
 
+	icon_enabled_resource, err := fyne.LoadResourceFromPath(icon_file_enabled)
+	if err != nil {
+		log.Printf("Error loading icon_enabled: %v", err)
+	}
+	icon_disabled_resource, err := fyne.LoadResourceFromPath(icon_file_disabled)
+	if err != nil {
+		log.Printf("Error loading icon_disabled: %v", err)
+	}
+
+	// icon_enabled := widget.NewIcon(icon_disabled_resource)
+	// icon_disabled := widget.NewIcon(icon_enabled_resource)
+
 	log.Printf("InitTray\n")
-	runtime.LockOSThread()
+	a := app.New()
 
-	gtk.Init(nil)
+	if desk, ok := a.(desktop.App); ok {
+		m := fyne.NewMenu("MyApp",
+			fyne.NewMenuItem("Enable/Disable", func() {
+				if inputEventCollector.Enabled {
+					inputEventCollector.Enabled = false
+					desk.SetSystemTrayIcon(icon_disabled_resource)
+					a.SetIcon(icon_disabled_resource)
 
-	// Create an instance of glib.Application
+				} else {
+					inputEventCollector.Enabled = true
+					desk.SetSystemTrayIcon(icon_enabled_resource)
+					a.SetIcon(icon_enabled_resource)
+
+				}
+
+			}))
+		desk.SetSystemTrayMenu(m)
+		desk.SetSystemTrayIcon(icon_enabled_resource)
+		a.SetIcon(icon_enabled_resource)
+
+	}
+
+	// w.ShowAndRun()
+	a.Run()
 	// application := glib.ApplicationNew("com.example.app", glib.APPLICATION_FLAGS_NONE)
 
-	indicator := appindicator.New("Tracker", "indicator-messages", appindicator.CategoryApplicationStatus)
-	indicator.SetStatus(appindicator.StatusActive)
-
-	icon, err := gtk.Ic
-	if err != nil {
-		log.Fatal("Nem sikerült létrehozni az ikont: ", err)
-	}
-
-
-	menu, err := gtk.MenuNew()
-	if err != nil {
-		log.Fatal("Unable to create menu:", err)
-	}
-
-	item, err := gtk.MenuItemNewWithLabel("enable/disable")
-	if err != nil {
-		log.Fatal("Unable create menu item")
-	}
-	item.Connect("activate", func() {
-
-		if inputEventCollector.Enabled {
-			inputEventCollector.Enabled = false
-			log.Printf("InputEventCollector disabled\n")
-		} else {
-			inputEventCollector.Enabled = true
-			log.Printf("InputEventCollector enabled\n")
-		}
-		SetIcon(indicator, inputEventCollector.Enabled)
-	})
-	menu.Append(item)
-
-	item, err = gtk.MenuItemNewWithLabel("Quit")
-	if err != nil {
-		log.Fatal("Unable create menu item")
-	}
-	item.Connect("activate", func() {
-		log.Printf("Quit Application Menuitem pressed, sent stop\n")
-		close(quitChan)
-	})
-	menu.Append(item)
-
-	menu.ShowAll()
-	indicator.SetMenu(menu)
-	SetIcon(indicator, inputEventCollector.Enabled)
-
-	// quit handler
 	go func() {
 		<-quitChan
 
 		log.Printf("Stop signal received in GTK quit handler\n")
+		a.Quit()
+		wg.Done()
 		// call quit when the main loop is idle
-		glib.IdleAdd(func() {
-			log.Printf("GTK main quit calling from IdleAdd\n")
-			gtk.MainQuit()
+		// glib.IdleAdd(func() {
+		// 	log.Printf("GTK main quit calling from IdleAdd\n")
+		// 	gtk.MainQuit()
 
-		})
+		// })
 	}()
 
 	log.Printf("Indicator created")
 	go func() {
 		log.Printf("GTK main started\n")
-		gtk.Main()
-		wg.Done()
+
 		log.Printf("GTK main finished\n")
 
 	}()
